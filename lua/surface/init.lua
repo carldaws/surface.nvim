ffd6ece9912facecda6d24517d74e126db778efa
Carl Dawson <carldawson@hey.com>
2026-03-31 10:27:15 +0100

    Adds normal mode keymap to hide surface window

--- lua/surface/init.lua
+++ lua/surface/init.lua
@@ -4,169 +4,175 @@ M.mappings = {}
 M.terminals = {}
 
 function M.setup(opts)
-	M.mappings = opts.mappings or {}
-	local default_position = opts.default_position or "center"
+  M.mappings = opts.mappings or {}
+  local default_position = opts.default_position or "center"
 
-	for _, mapping in ipairs(M.mappings) do
-		local starting_position = mapping.position or default_position
-		vim.api.nvim_set_keymap(
-			"n",
-			mapping.keymap,
-			string.format(
-				"<cmd>lua require('surface').open('%s', '%s')<CR>",
-				vim.fn.escape(mapping.command, "'"),
-				vim.fn.escape(starting_position, "'")
-			),
-			{ noremap = true, silent = true }
-		)
-	end
+  for _, mapping in ipairs(M.mappings) do
+    local starting_position = mapping.position or default_position
+    vim.api.nvim_set_keymap(
+      "n",
+      mapping.keymap,
+      string.format(
+        "<cmd>lua require('surface').open('%s', '%s')<CR>",
+        vim.fn.escape(mapping.command, "'"),
+        vim.fn.escape(starting_position, "'")
+      ),
+      { noremap = true, silent = true }
+    )
+  end
 end
 
 function M.open(command, position)
-	local terminal_data = M.terminals[command]
-	local buffer = terminal_data and terminal_data.buffer
+  local terminal_data = M.terminals[command]
+  local buffer = terminal_data and terminal_data.buffer
 
-	if not buffer or not vim.api.nvim_buf_is_valid(buffer) then
-		buffer = vim.api.nvim_create_buf(false, true)
-		M.terminals[command] = { buffer = buffer, position = position }
-	end
+  if not buffer or not vim.api.nvim_buf_is_valid(buffer) then
+    buffer = vim.api.nvim_create_buf(false, true)
+    M.terminals[command] = { buffer = buffer, position = position }
+  end
 
-	local window_config = M.window_config_for(M.terminals[command])
-	local window = vim.api.nvim_open_win(buffer, true, window_config)
+  local window_config = M.window_config_for(M.terminals[command])
+  local window = vim.api.nvim_open_win(buffer, true, window_config)
 
-	M.terminals[command].window = window
+  M.terminals[command].window = window
 
-	if vim.fn.bufname(buffer) == "" then
-		local shell = os.getenv("SHELL") or "/bin/sh"
-		local job_id = vim.fn.jobstart({ shell, "-c", command .. "; exec " .. shell }, { term = true })
-		vim.b[buffer].terminal_job_id = job_id
+  if vim.fn.bufname(buffer) == "" then
+    local shell = os.getenv("SHELL") or "/bin/sh"
+    local job_id = vim.fn.jobstart({ shell, "-c", command .. "; exec " .. shell }, { term = true })
+    vim.b[buffer].terminal_job_id = job_id
 
-		vim.api.nvim_buf_set_keymap(
-			buffer,
-			"t",
-			"<Esc><Esc>",
-			"<C-\\><C-n>:lua require('surface').hide('" .. command .. "')<CR>",
-			{ noremap = true, silent = true }
-		)
+    vim.api.nvim_buf_set_keymap(
+      buffer,
+      "n",
+      "<Esc><Esc>",
+      ":lua require('surface').hide('" .. command .. "')<CR>",
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      buffer,
+      "t",
+      "<Esc><Esc>",
+      "<C-\\><C-n>:lua require('surface').hide('" .. command .. "')<CR>",
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      buffer,
+      "t",
+      "<Esc><Left>",
+      string.format("<C-\\><C-n>:lua require('surface').move('%s', 'left')<CR>i", vim.fn.escape(command, "'")),
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      buffer,
+      "t",
+      "<Esc><Down>",
+      string.format("<C-\\><C-n>:lua require('surface').move('%s', 'bottom')<CR>i", vim.fn.escape(command, "'")),
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      buffer,
+      "t",
+      "<Esc><Up>",
+      string.format("<C-\\><C-n>:lua require('surface').move('%s', 'top')<CR>i", vim.fn.escape(command, "'")),
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      buffer,
+      "t",
+      "<Esc><Right>",
+      string.format("<C-\\><C-n>:lua require('surface').move('%s', 'right')<CR>i", vim.fn.escape(command, "'")),
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_buf_set_keymap(
+      buffer,
+      "t",
+      "<Esc>c",
+      string.format("<C-\\><C-n>:lua require('surface').move('%s', 'center')<CR>i", vim.fn.escape(command, "'")),
+      { noremap = true, silent = true }
+    )
+  end
 
-		vim.api.nvim_buf_set_keymap(
-			buffer,
-			"t",
-			"<Esc><Left>",
-			string.format("<C-\\><C-n>:lua require('surface').move('%s', 'left')<CR>i", vim.fn.escape(command, "'")),
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_buf_set_keymap(
-			buffer,
-			"t",
-			"<Esc><Down>",
-			string.format("<C-\\><C-n>:lua require('surface').move('%s', 'bottom')<CR>i", vim.fn.escape(command, "'")),
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_buf_set_keymap(
-			buffer,
-			"t",
-			"<Esc><Up>",
-			string.format("<C-\\><C-n>:lua require('surface').move('%s', 'top')<CR>i", vim.fn.escape(command, "'")),
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_buf_set_keymap(
-			buffer,
-			"t",
-			"<Esc><Right>",
-			string.format("<C-\\><C-n>:lua require('surface').move('%s', 'right')<CR>i", vim.fn.escape(command, "'")),
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_buf_set_keymap(
-			buffer,
-			"t",
-			"<Esc>c",
-			string.format("<C-\\><C-n>:lua require('surface').move('%s', 'center')<CR>i", vim.fn.escape(command, "'")),
-			{ noremap = true, silent = true }
-		)
-	end
-
-	vim.api.nvim_command("startinsert")
+  vim.api.nvim_command("startinsert")
 end
 
 function M.move(command, position)
-	local terminal = M.terminals[command]
-	M.terminals[command].position = position
+  local terminal = M.terminals[command]
+  M.terminals[command].position = position
 
-	local window_config = M.window_config_for(terminal)
-	vim.api.nvim_win_set_config(terminal.window, window_config)
+  local window_config = M.window_config_for(terminal)
+  vim.api.nvim_win_set_config(terminal.window, window_config)
 end
 
 function M.get_available_dimensions()
-	local ui = vim.api.nvim_list_uis()[1]
-	if not ui then
-		return vim.o.columns, vim.o.lines
-	end
+  local ui = vim.api.nvim_list_uis()[1]
+  if not ui then
+    return vim.o.columns, vim.o.lines
+  end
 
-	local available_width = ui.width
-	local available_height = ui.height
+  local available_width = ui.width
+  local available_height = ui.height
 
-	-- Account for command line
-	available_height = available_height - vim.o.cmdheight
+  -- Account for command line
+  available_height = available_height - vim.o.cmdheight
 
-	-- Account for status line
-	if vim.o.laststatus > 0 then
-		available_height = available_height - 1
-	end
+  -- Account for status line
+  if vim.o.laststatus > 0 then
+    available_height = available_height - 1
+  end
 
-	-- Account for tab line
-	if vim.o.showtabline > 0 or (vim.o.showtabline == 1 and #vim.api.nvim_list_tabpages() > 1) then
-		available_height = available_height - 1
-	end
+  -- Account for tab line
+  if vim.o.showtabline > 0 or (vim.o.showtabline == 1 and #vim.api.nvim_list_tabpages() > 1) then
+    available_height = available_height - 1
+  end
 
-	return available_width, available_height
+  return available_width, available_height
 end
 
 function M.window_config_for(terminal)
-	local width, height = M.get_available_dimensions()
+  local width, height = M.get_available_dimensions()
 
-	local window_config = {
-		style = "minimal",
-		relative = "editor",
-		border = "rounded",
-	}
+  local window_config = {
+    style = "minimal",
+    relative = "editor",
+    border = "rounded",
+  }
 
-	if terminal.position == "bottom" then
-		window_config.width = width - 2
-		window_config.height = math.floor(height / 2) - 1
-		window_config.row = height - window_config.height - 1
-		window_config.col = 1
-	elseif terminal.position == "top" then
-		window_config.width = width - 2
-		window_config.height = math.floor(height / 2) - 1
-		window_config.row = 0
-		window_config.col = 1
-	elseif terminal.position == "left" then
-		window_config.width = math.floor(width / 2) - 1
-		window_config.height = height - 1
-		window_config.row = 0
-		window_config.col = 0
-	elseif terminal.position == "right" then
-		window_config.width = math.floor(width / 2) - 1
-		window_config.height = height - 1
-		window_config.row = 0
-		window_config.col = width - window_config.width - 1
-	elseif terminal.position == "center" then
-		window_config.width = math.floor(width * 0.8)
-		window_config.height = math.floor(height * 0.8)
-		window_config.row = math.floor((height - window_config.height) / 2)
-		window_config.col = math.floor((width - window_config.width) / 2)
-	end
+  if terminal.position == "bottom" then
+    window_config.width = width - 2
+    window_config.height = math.floor(height / 2) - 1
+    window_config.row = height - window_config.height - 1
+    window_config.col = 1
+  elseif terminal.position == "top" then
+    window_config.width = width - 2
+    window_config.height = math.floor(height / 2) - 1
+    window_config.row = 0
+    window_config.col = 1
+  elseif terminal.position == "left" then
+    window_config.width = math.floor(width / 2) - 1
+    window_config.height = height - 1
+    window_config.row = 0
+    window_config.col = 0
+  elseif terminal.position == "right" then
+    window_config.width = math.floor(width / 2) - 1
+    window_config.height = height - 1
+    window_config.row = 0
+    window_config.col = width - window_config.width - 1
+  elseif terminal.position == "center" then
+    window_config.width = math.floor(width * 0.8)
+    window_config.height = math.floor(height * 0.8)
+    window_config.row = math.floor((height - window_config.height) / 2)
+    window_config.col = math.floor((width - window_config.width) / 2)
+  end
 
-	return window_config
+  return window_config
 end
 
 function M.hide(command)
-	local terminal = M.terminals[command]
-	if terminal and terminal.window and vim.api.nvim_win_is_valid(terminal.window) then
-		vim.api.nvim_win_close(terminal.window, true)
-		M.terminals[command].window = nil
-	end
+  local terminal = M.terminals[command]
+  if terminal and terminal.window and vim.api.nvim_win_is_valid(terminal.window) then
+    vim.api.nvim_win_close(terminal.window, true)
+    M.terminals[command].window = nil
+  end
 end
 
 return M
